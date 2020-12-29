51797f58ccd1aba24ade6916b43ab04b24335adc
Flyken <39961800+Flyken271@users.noreply.github.com>
2020-12-29 11:07:03 -0500

    stuff

--- components/firebase.js
+++ components/firebase.js
@@ -6,13 +6,7 @@ import 'firebase/firestore'
 
 
     var firebaseConfig = {
-        apiKey: "AIzaSyCV7exMa9ym17d4Mo0NDEztiSEKA-1Hjsw",
-        authDomain: "lucid-7f393.firebaseapp.com",
-        projectId: "lucid-7f393",
-        storageBucket: "lucid-7f393.appspot.com",
-        messagingSenderId: "145251018136",
-        appId: "1:145251018136:web:5f74dedaf47de0acf0f7ed",
-        measurementId: "G-MF6QYMCWGJ"
+        
       };
       // Initialize Firebase
 if(!firebase.apps.length){
